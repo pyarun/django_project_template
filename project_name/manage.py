551874b68836adb531072f5a32a37e13971b498e
Arun Mittal <mittal.talk@gmail.com>
2013-10-07 01:34:11 +0530

    changed project_name value to context variables so that users given project name can be used

--- project_name/manage.py
+++ project_name/manage.py
@@ -5,7 +5,7 @@ import sys, socket
 hostname = socket.gethostname()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djcms.settings.local_{}".format(hostname.replace("-", "")))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.local_{}".format(hostname.replace("-", "")))
 
     from django.core.management import execute_from_command_line
 
